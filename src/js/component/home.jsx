4eae2525e1b824157b753b79e606535d21993ead
Jose Miguel <josemi_garcia_seven@hotmail.com>
2024-11-18 19:57:42 +0000

    Proyecto terminado con luz morada.

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+    const [ color, setColor] = useState("null");
+	const [showPurple, setShowPurple] = useState(false);
+	
+
+	const addPurpleLight = () => {
+		setShowPurple((prev => !prev)); 
+	};
+ 
+     return (
+         <div className="container">
+             
+             <div className = "traffic-light">
+                 <div onClick={() => setColor("red")} className={"light red" + (color === "red" ? " glow" : "" )}></div>
+                 <div onClick={() => setColor("yellow")} className={"light yellow" + (color === "yellow" ? " glow" : "" )}></div>
+                 <div onClick={() => setColor("green")} className={"light green" + (color === "green" ? " glow" : "" )}></div>
+				 {showPurple && (
+				<div onClick={() => setColor("purple")} className={"light purple" + (color === "purple" ? " glow" : "" )}></div>
+				)}
+			</div>
+			<div className="button">
+				<button onClick={addPurpleLight}>Purple Light</button>
+			</div>
 		</div>
 	);
 };
 
 export default Home;
+         
